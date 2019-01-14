27e8aa60a74f78d6df4dfae8f2ed4901dc3a1efc
amusing-bolt-9198 <edmund.nelson.b@gmail.com>
2019-01-14 03:56:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def badge_maker(guy)
+  return "Hello, my name is #{guy}."
+end
+def batch_badge_creator(names)
+  final = []
+  names.each do |name|
+    final.push(badge_maker(name))
+  end
+end
